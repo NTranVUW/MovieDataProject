74b2c2c16d2e70d6d39db9cdece069ff82ff72f6
Nick <NTranVUW@gmail.com>
2019-12-18 18:08:48 +1300

    Successfully scrapes wikipedia for every movie title for each year (2010-2018), including information about: 1. title, 2. wikipedia link, 3. imdb link, 4. metacritic link, 5. rotten tomatoes link and 6. boxofficemojo link. Data is then written to json files representing each year with each json object presenting each film. The program now reads from the json files into python objects.

--- movie.py
+++ movie.py
@@ -5,25 +5,30 @@ import requests
 class Movie:
     def __init__(self, title, wiki_link):
         self.title = title
-        self.wiki_link = wiki_link
+        self.wiki_link = "https://en.wikipedia.org" + wiki_link
         self.imdb_link = None
         self.metacritic_link = None
         self.rottentomatoes_link = None
-        self.boxofficemofo_link = None
+        self.boxofficemojo_link = None
 
     def get_wiki_external_links(self):
-        url = "https://en.wikipedia.org" + self.wiki_link
+        url = self.wiki_link
         req = requests.get(url).text
         soup = BeautifulSoup(req, 'html.parser')
         for item in soup.find_all('a', class_="external text"):
             site = item['href']
-            site_name = site.split('.')[1]
+            site_split = site.split('.')
+            if len(site_split) > 1:
+                site_name = site_split[1]
             if site_name == "imdb":
-                self.imdb_link = site
+                if site_split[2].startswith("com/title/"):
+                    self.imdb_link = site
             elif site_name == "metacritic":
-                self.metacritic_link = site
-                print(site)
+                if site_split[2].startswith("com/movie/"):
+                    self.metacritic_link = site
             elif site_name == "rottentomatoes":
-                self.rottentomatoes_link = site
+                if site_split[2].startswith("com/m/"):
+                    self.rottentomatoes_link = site
             elif site_name == "boxofficemojo":
-                self.boxofficemofo_link = site
+                if site_split[2].startswith("com/movies/"):
+                    self.boxofficemojo_link = site
--- movie_lister.py
+++ movie_lister.py
@@ -1,4 +1,8 @@
 import wikipedia_parser
+import time
+import json
+
+from movie import Movie
 
 year_dict = {}
 
@@ -8,11 +12,41 @@ class Year:
     def __init__(self, year):
         self.year = year
         self.films = {}
-        self.get_films()
+        self.parse_film_data()
+        # self.get_films()
+        # self.print_to_json()
 
     def get_films(self):
         wikipedia_parser.parse(self.year, self.films)
-        self.films["Black Panther"].get_wiki_external_links()
+        for film in self.films:
+            self.films[film].get_wiki_external_links()
+
+    def print_to_json(self):
+        data = {'films': []}
+        for film_name in self.films:
+            film = self.films[film_name]
+            data["films"].append({
+                'title': film.title,
+                'wikipedia': film.wiki_link,
+                'imdb': film.imdb_link,
+                'metacritic': film.metacritic_link,
+                'rottentomatoes': film.rottentomatoes_link,
+                'boxofficemojo': film.boxofficemojo_link
+            })
+        with open('resources\\' + str(self.year) + '_data.json', 'w') as file:
+            json.dump(data, file)
+
+    def parse_film_data(self):
+        with open('resources//' + str(self.year) + '_data.json') as json_file:
+            data = json.load(json_file)
+            for film in data["films"]:
+                film_title = film["title"]
+                film_obj = Movie(film_title, film["wikipedia"])
+                film_obj.imdb_link = film["imdb"]
+                film_obj.metacritic_link = film["metacritic"]
+                film_obj.rottentomatoes_link = film["rottentomatoes"]
+                film_obj.boxofficemojo_link = film["boxofficemojo"]
+                self.films[film_title] = film_obj
 
 
 def create_years():
@@ -22,5 +56,4 @@ def create_years():
 
 
 if __name__ == '__main__':
-    # create_years()
-    year_dict[str(2018)] = Year(str(2018))
+    create_years()
